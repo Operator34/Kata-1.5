ca42598be71f990e7341c8b2c2d5660818f41a35
Andrey Loginov <operator34@yandex.ru>
2023-08-30 18:11:58 +0300

    1.5.4

--- script2.js
+++ script2.js
@@ -1,3 +1,44 @@
+const buttonReadMoreBrand = document.querySelector(".button-read-more");
+const buttonHiddenBrand = document.querySelector(".hidden-more");
+const allSliderElementBrand = document.querySelectorAll(".swiper-slide");
+let sliderElementBrandHiddenShow = [];
+const mediaQuery_768px = window.matchMedia(
+    "(min-width: 768px) and (max-width: 1120px)"
+);
+
+const mediaQuery_1121px = window.matchMedia("(min-width: 1121px)");
+
+if (mediaQuery_768px.matches) {
+    sliderElementBrandHiddenShow = document.querySelectorAll(
+        ".swiper-slide:nth-last-child(-n+5)"
+    );
+}
+if (mediaQuery_1121px.matches) {
+    sliderElementBrandHiddenShow = document.querySelectorAll(
+        ".swiper-slide:nth-last-child(-n+3)"
+    );
+}
+
+//Слушатель кнопки показать все
+buttonReadMoreBrand.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log("press");
+    buttonReadMoreBrand.classList.add("hidden");
+    buttonHiddenBrand.classList.remove("hidden");
+    for (let i = 0; i < allSliderElementBrand.length; i++) {
+        allSliderElementBrand[i].classList.add("show");
+    }
+});
+//Слушатель кнопки скрыть
+buttonHiddenBrand.addEventListener("click", function () {
+    buttonHiddenBrand.classList.add("hidden");
+    buttonReadMoreBrand.classList.remove("hidden");
+    for (let i = 0; i < sliderElementBrandHiddenShow.length; i++) {
+        sliderElementBrandHiddenShow[i].classList.remove("show");
+    }
+});
+
+//Слайдер
 window.addEventListener("DOMContentLoaded", () => {
     const resizableSwiper = (
         breakpoint,
